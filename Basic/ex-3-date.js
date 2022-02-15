03db06aa595cf2467a3efebe5896fd517dee937e
Amir <amirhatam136@gmail.com>
2022-02-15 22:45:26 +0100

    Get today with Date Methods

--- Basic/ex-3-date.js
+++ Basic/ex-3-date.js
@@ -2,9 +2,26 @@
 // Expected Output :
 // mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 
+//First Solution with package moment
 const moment = require("moment")
 
-const date = moment().format('MM/DD/YYYY');
+const dateMoment = moment().format('MM/DD/YYYY');
 // const date = moment().format('DD MM YYYY');
 
-console.log("Today is", date);
+// console.log("Today is", dateMoment);
+
+//Second Solution with Date Methods
+let today = new Date();
+let dd = today.getDate()
+let mm = today.getMonth() + 1;
+const yyyy = today.getFullYear();
+
+if (dd < 10) {
+    dd = `0${dd}`
+}
+
+if (mm < 10) {
+    mm = `0${mm}`
+}
+today = `${dd}/${mm}/${yyyy}`
+console.log("Today is", today);
