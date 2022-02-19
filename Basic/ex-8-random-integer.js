487b200c83e46f40a19ac6465f2a8653f9ba3d64
Amir <amirhatam136@gmail.com>
2022-02-19 16:14:54 +0100

    ex-8, way two

--- Basic/ex-8-random-integer.js
+++ Basic/ex-8-random-integer.js
@@ -1,13 +1,15 @@
+var prompt = require("prompt");
 /*  Write a JavaScript program where the program takes a random integer 
  between 1 to 10, the user is then prompted to input a guess number. 
  If the user input matches with guess number, the program will 
  display a message "Good Work" otherwise display a message "Not matched". */
 
+prompt.start();
 
+
+//First Solution with arrow function
 const min = 1
 const max = 10
-
-
 
 const checkValue = (userNum) => {
     const random = Math.floor(Math.random() * (max - min + 1) + min)
@@ -20,5 +22,21 @@ const checkValue = (userNum) => {
     }
 }
 
+// checkValue(2)
 
-checkValue(2)
+
+//Second Solution with 
+
+const num = Math.ceil(Math.random() * 10);
+console.log(num);
+
+const gnum = prompt('Guess the number between 1 and 10 inclusive')
+// if (gnum == num) {
+//     console.log("Good Work");
+// }
+// else {
+//     console.log("Not matched");
+
+// }
+
+console.log(gnum);
